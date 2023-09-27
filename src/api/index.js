226957bc4526002f0c09de0f88b3180a2d69eb40
sou <c@sou.best>
2023-09-27 15:29:32 +0800

    timeout and retry

--- src/api/index.js
+++ src/api/index.js
@@ -6,9 +6,31 @@ import axios from "@/api/request";
  * @param {boolean} isNew 是否拉取最新数据
  * @returns
  */
-export const getHotLists = (type, isNew) => {
-  return axios({
-    method: "GET",
-    url: `/${type}${isNew ? "/new" : "/"}`,
-  });
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function fetchDataWithRetry(apiEndpoint, maxRetries = 3, delay = 3000) {
+    let retries = 0;
+
+    while (retries < maxRetries) {
+        try {
+            const data = await axios.get(apiEndpoint, { timeout: 6000 });  // 5 seconds timeout
+            return data;  // Directly return the data since the response interceptor in "@/api/request" only returns response.data
+        } catch (error) {
+            console.error(`Attempt ${retries + 1} failed. Retrying...`);
+            retries++;
+            if (retries < maxRetries) {
+                await sleep(delay);
+            } else {
+                throw new Error("Max retries reached");
+            }
+        }
+    }
+}
+
+export const getHotLists = async (type, isNew) => {
+    const endpoint = `/${type}${isNew ? "/new" : "/"}`;
+    return await fetchDataWithRetry(endpoint);
 };
